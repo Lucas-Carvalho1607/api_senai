848b490d4e0d0953d744d3d48a51593e949b560e
Lucas Carvalho Lima <PET0105386@aluno.firjansenaisesi.com.br>
2023-10-02 09:21:19 -0300

    Primeiro commit

---
+++ src/main/java/com/api_senai/api/ApiApplication.java
@@ -0,0 +1,19 @@
+package com.api_senai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+
+		System.out.println("Hello World");
+
+	}
+
+
+
+}
